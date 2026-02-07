2c8b7bad8cc171b6f499174906070e46be6d9b74
Sam-239 <52288646+Sam-239@users.noreply.github.com>
2026-02-07 21:13:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function showMessage(response) {
     yesMessage.textContent = "See you on the 14th my Princess";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "./dance.gif";
 
     // Remove the "Yes" button
     document.getElementById("yesButton").remove();
